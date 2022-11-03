bb38bfc686e5ddeedf3ea675037ce390c2bfad37
Kellen Parker <kellenrparker@gmail.com>
2022-11-03 08:22:11 -0500

    fixes to help dialog

--- src/components/Help.js
+++ src/components/Help.js
@@ -2,7 +2,7 @@ import "./styles/Dialog.css";
 import "./styles/Help.css";
 
 const Help = (props) => {
-    const br = <br className="helpbr"/>;
+    const br = <br className="helpbr" />;
     return (
         <div className="dialogCont" onClick={props.toggle}>
             <div className="dialogBoxHelp" onClick={(e) => e.stopPropagation()}>
@@ -14,17 +14,39 @@ const Help = (props) => {
                     list of hints are as follows:
                 </p>
                 <div className="hintCont">
-                    <p className="desc hint">{"1. Greater than 25 years away "}{br}{"( > 25 years )"}</p>
-                    <p className="desc hint">{"2. Less than 25 years away "}{br}{"( < 25 years )"}</p>
-                    <p className="desc hint">{"3. Less than 15 years away "}{br}{"( < 15 years )"}</p>
-                    <p className="desc hint">{"4. Less than 5 years away "}{br}{"( < 5 years )"}</p>
+                    <p className="desc hint">
+                        {"1. Greater than 25 years away "}
+                        {br}
+                        {"( > 25 years )"}
+                    </p>
+                    <p className="desc hint">
+                        {"2. Less than 25 years away "}
+                        {br}
+                        {"( < 25 years )"}
+                    </p>
+                    <p className="desc hint">
+                        {"3. Less than 15 years away "}
+                        {br}
+                        {"( < 15 years )"}
+                    </p>
+                    <p className="desc hint">
+                        {"4. Less than 5 years away "}
+                        {br}
+                        {"( < 5 years )"}
+                    </p>
                 </div>
                 <p className="desc">You can also keep track of your stats by pressing the stats button.</p>
                 <div className="endCont">
                     <p className="desc-end">Made by: Kellen Parker</p>
-                    <p className="desc-end-link">~ GitHub ~</p>
+                    <a className="gita" href="https://github.com/Kellenparker/mapple" target="_blank" rel="noreferrer">
+                        <button className="gitb" onClick={props.toggleStats}>
+                            ~ Github ~
+                        </button>
+                    </a>
                 </div>
-                <button id="close" onClick={props.toggle}>Close</button>
+                <button id="close" onClick={props.toggle}>
+                    Close
+                </button>
             </div>
         </div>
     );
